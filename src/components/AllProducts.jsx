7e9b6d6d9993652938106f70529064751cebc011
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-08-25 17:51:59 -0500

    rendered all product titles - updates to come

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -1,3 +1,29 @@
+import { useEffect, useState } from "react";
+
 export default function AllProducts() {
-  return <h1>All Products</h1>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const result = await response.json();
+        setProducts(result);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <h1>All Products</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
