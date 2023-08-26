d5a81429fe54265fd3082a1558c2f3edca76e498
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-08-26 18:30:03 -0500

    added route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
 import AllProducts from "./components/AllProducts";
 
+import "./App.css";
+
 function App() {
-  return <AllProducts />;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<AllProducts />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
