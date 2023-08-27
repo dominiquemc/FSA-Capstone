4cd7b02568a38bb2178717c98c1d561bd7356e7b
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-08-27 00:35:42 -0500

    changed route path for AllProducts

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,8 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/products/:productId" component={<SingleProduct />} />
+        <Route path="/" element={<AllProducts />} />
+        <Route path="/products/:productId" element={<SingleProduct />} />
       </Routes>
     </>
   );
