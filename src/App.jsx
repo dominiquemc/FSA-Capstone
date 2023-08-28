a98a86aa249d1a3771502249b2efa7689e58bed5
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-08-28 16:25:02 -0500

    added Login component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import AllProducts from "./components/AllProducts";
 import SingleProduct from "./components/SingleProduct";
-
+import Login from "./components/Login";
 import "./App.css";
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/" element={<AllProducts />} />
         <Route path="/products/:productId" element={<SingleProduct />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
