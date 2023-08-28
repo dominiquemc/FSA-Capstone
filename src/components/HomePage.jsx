9b20d684902cbe41f6919fc19bb4b5da77c5c654
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-08-28 16:26:19 -0500

    added div class and conditional for show products

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,18 +1,22 @@
 import Navigation from "./Navigation";
 import AllProducts from "./AllProducts";
+import Login from "./Login";
+import { useState } from "react";
 
 export default function HomePage() {
+  const [showLogin, setShowLogin] = useState(false);
   return (
-    <>
-      <Navigation />
-      <div className="home">
-        <img
-          src="./src/assets/banner.png"
-          className="siteBanner"
-          alt="Shop Direct Banner"
-        />
+    <div className="home-container">
+      <div className="navigation">
+        <Navigation setShowLogin={setShowLogin} />
       </div>
-      <AllProducts />
-    </>
+      <img
+        src="./src/assets/banner.png"
+        className="siteBanner"
+        alt="Shop Direct Banner"
+      />
+
+      <div className="home">{showLogin ? <Login /> : <AllProducts />}</div>
+    </div>
   );
 }
