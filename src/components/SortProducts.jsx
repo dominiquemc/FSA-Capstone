dbef822236c2e8b98a2c61714177f919a5c3797f
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-08-28 19:29:15 -0500

    complete sortbar

--- src/components/SortProducts.jsx
+++ src/components/SortProducts.jsx
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "react";
 
-export default function SortProducts() {
-  const [sortedProducts, setSortedProducts] = useState([]);
+export default function SortProducts({ setSortedProducts }) {
+  const [sortMethod, setSortMethod] = useState("none");
+
+  const sortMethods = {
+    none: { method: (a, b) => 0 },
+    ascending: { method: (a, b) => a.localeCompare(b) },
+    descending: { method: (a, b) => b.localeCompare(a) },
+  };
 
   useEffect(() => {
     const fetchSortedProducts = async () => {
       try {
         const response = await fetch(
-          `https://fakestoreapi.com/products?sort=${sortedProducts}`
+          `https://fakestoreapi.com/products?sort=${sortMethod}`
         );
         const result = await response.json();
         setSortedProducts(result);
@@ -16,8 +22,21 @@ export default function SortProducts() {
         console.log(err);
       }
     };
-    fetchSortedProducts();
-  }, []);
+    if (sortMethod !== "none") {
+      fetchSortedProducts();
+    }
+  }, [sortMethod, setSortedProducts]);
 
-  return <div className="sortBar"></div>;
+  return (
+    <div className="sortbar">
+      <label>Sort by:</label>
+      <select
+        value={sortMethod}
+        onChange={(e) => setSortMethod(e.target.value)}
+      >
+        <option value="asc">Ascending (a-z)</option>
+        <option value="desc">Descending (z-a)</option>
+      </select>
+    </div>
+  );
 }
