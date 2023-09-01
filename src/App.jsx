01ede2849c4f5881c6d99899143e621ba0b3aa1d
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-08-31 22:23:28 -0500

    moved navigation component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,22 @@ import SingleProduct from "./components/SingleProduct";
 import Login from "./components/Login";
 import "./App.css";
 import Footer from "./components/Footer";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <>
+      <Navigation />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/" element={<AllProducts />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <HomePage />
+              <AllProducts />
+            </>
+          }
+        />
         <Route path="/products/:productId" element={<SingleProduct />} />
         <Route path="/login" element={<Login />} />
       </Routes>
