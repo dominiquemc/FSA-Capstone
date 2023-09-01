ec64769628e67b2472cf462d33a1fc24e7614a4f
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-09-01 13:30:01 -0500

    added searchbar logic

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,25 @@
-export default function Searchbar() {
-  <div className="searchbar">
-    <input type="search" placeholder="Search for a product" />
-  </div>;
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import AllProducts from "./AllProducts";
+
+export default function Searchbar({ filterProducts }) {
+  const [query, setQuery] = useState("");
+
+  const handleChange = (value) => {
+    setQuery(value);
+    filterProducts(value);
+  };
+  return (
+    <div className="inputWrapper">
+      <FaSearch id="search" />
+      <input
+        type="searchbar"
+        name="searchbar"
+        id="searchbar"
+        placeholder="Search for a product..."
+        value={query}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+    </div>
+  );
 }
