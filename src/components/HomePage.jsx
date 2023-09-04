1aeb7d3886383d66a02fc0fad9c104d4b5954b0e
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-09-04 02:58:38 -0500

    removed unused hooks

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,6 @@
-import Navigation from "./Navigation";
-import AllProducts from "./AllProducts";
-import Login from "./Login";
 import { useState } from "react";
 
 export default function HomePage() {
-  const [showLogin, setShowLogin] = useState(false);
   return (
     <div className="home-container">
       <img
@@ -12,8 +8,6 @@ export default function HomePage() {
         className="siteBanner"
         alt="Shop Direct Banner"
       />
-
-      {/* <div className="home">{showLogin ? <Login /> : <AllProducts />}</div> */}
     </div>
   );
 }
