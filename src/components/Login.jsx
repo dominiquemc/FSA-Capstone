4722f232f7fb4318a9095bcf0ec5f30c044e6e33
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-09-04 02:53:51 -0500

    utilized materialUI for login form, and ensured that handleSubmit was fetching login API details

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,35 @@
 import { useState } from "react";
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 export default function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const defaultTheme = createTheme();
+
   const fetchLogin = async () => {
     try {
       const response = await fetch("https://fakestoreapi.com/auth/login", {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify({
-          username: "mor_2314",
-          password: "83r5^_",
+          username: username,
+          password: password,
         }),
       });
 
@@ -22,38 +41,88 @@ export default function Login() {
     }
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const loginResponse = await fetchLogin(username, password);
+    console.log(loginResponse);
+  };
+
   return (
-    <div className="login-container">
-      <img
-        src="../src/assets/banner.png"
-        className="siteBanner"
-        alt="Shop Direct Banner"
-      />
-      <div className="login-content">
-        <h1>Login</h1>
-        <form>
-          <label>
-            Username:
-            <input
-              type="text"
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
+              autoComplete="username"
+              autoFocus
             />
-          </label>
-
-          <label>
-            Password:
-            <input
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
               type="password"
+              id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              autoComplete="current-password"
             />
-          </label>
-          <div className="login-btn">
-            <button>Sign In</button>
-          </div>
-        </form>
-      </div>
-    </div>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
