7c43a942165027bdccd32fc9296ca619f1023e97
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-09-13 22:53:01 -0500

    added cart state to App.jsx to be shared across components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,22 +12,41 @@ import Women from "./components/WomensClothing";
 import Jewelry from "./components/Jewelry";
 import Register from "./components/Register";
 import Cart from "./components/Cart";
+import { useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    const existingProduct = cart.findIndex((item) => item.id === product.id);
+
+    if (existingProduct !== -1) {
+      const updateCart = [...cart];
+      updateCart[existingProduct].quantity += 1;
+      setCart(updateCart);
+    } else {
+      // add product to cart {
+      setCart([...cart, { ...product, quantity: 1 }]);
+    }
+  };
+
   return (
     <>
       <Navigation />
       <HomePage />
       <Routes>
         <Route path="/" element={<AllProducts />} />
-        <Route path="/products/:productId" element={<SingleProduct />} />
+        <Route
+          path="/products/:productId"
+          element={<SingleProduct addToCart={addToCart} />}
+        />
         <Route path="/electronics" element={<Electronics />} />
         <Route path="/jewelry" element={<Jewelry />} />
         <Route path="/men" element={<Men />} />
         <Route path="/women" element={<Women />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/cart" element={<Cart cart={cart} />} />
       </Routes>
       <Footer />
     </>
