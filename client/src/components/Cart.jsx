1ff7d43f44fa37188165f48468a271edfdaa2bb2
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-09-16 23:36:31 -0500

    initial cart set-up

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,3 +1,14 @@
-export default function Cart() {
-  return <h1>Cart</h1>;
+export default function Cart({ cart }) {
+  return (
+    <div>
+      <h2>ShoppingCart</h2>;
+      <ul>
+        {cart.map((item) => (
+          <li key={item.id}>
+            {item.title} - Quantity: {item.quantity}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
