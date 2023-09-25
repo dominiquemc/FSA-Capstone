e05c6d9bdc8e4c85560f069a444007223fef80cd
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-09-25 01:51:41 -0500

    added react bootstrap styles

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,5 +1,17 @@
 import { useEffect, useState } from "react";
 import { useCart } from "../CartContext";
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBCol,
+  MDBContainer,
+  MDBIcon,
+  MDBInput,
+  MDBRow,
+  MDBTypography,
+} from "mdb-react-ui-kit";
 
 export default function Cart() {
   const { cart, removeFromCart, clearCart } = useCart();
@@ -22,45 +34,187 @@ export default function Cart() {
   };
 
   return (
-    <div className="cart-container">
-      <h2 className="cart-title">Shopping Cart</h2>
-      {cart.length === 0 ? (
-        <p className="empty-cart-msg">Your cart is currently empty.</p>
-      ) : (
-        <div>
-          <ul className="cart-items">
-            {cart.map((item) => (
-              <li key={item.id} className="cart-item">
-                <div className="cart-item-details">
-                  <img
-                    src={item.image}
-                    alt={item.title}
-                    className="cart-item-image"
-                  />
-                  <p className="cart-item-title">{item.title}</p>
-                  <p className="cart-item-quantity">
-                    Quantity: {item.quantity}
-                  </p>
-                  <p className="cart-item-price">
-                    Price: ${item.price * item.quantity}
-                  </p>
-                  <button
-                    onClick={() => handleRemoveItem(item.id)}
-                    className="remove-item-btn"
-                  >
-                    Remove
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
-          <p className="cart-total">Total: ${cartTotal}</p>
-          <button onClick={handleClearCart} className="clear-cart-btn">
-            Clear Cart
-          </button>
-          <button>Checkout</button>
-        </div>
-      )}
-    </div>
+    <section className="h-100 h-custom" style={{ backgroundColor: "#eee" }}>
+      <MDBContainer className="py-5 h-100">
+        <MDBRow className="justify-content-center align-items-center h-100">
+          <MDBCol>
+            <MDBCard>
+              <MDBCardBody className="p-4">
+                <MDBRow>
+                  <MDBCol lg="7">
+                    <MDBTypography tag="h5">
+                      <a href="/" className="text-body">
+                        <MDBIcon fas icon="long-arrow-alt-left me-2" /> Continue
+                        shopping
+                      </a>
+                    </MDBTypography>
+
+                    <hr />
+
+                    <div className="d-flex justify-content-between align-items-center mb-4">
+                      <div>
+                        <p className="mb-1">Shopping cart</p>
+                        <p className="mb-0">{`You have ${cart.length} items in your cart`}</p>
+                      </div>
+                    </div>
+
+                    {/* Display cart items */}
+                    {cart.length === 0 ? (
+                      <p className="empty-cart-msg">
+                        Your cart is currently empty.
+                      </p>
+                    ) : (
+                      <div>
+                        <div className="cart-items">
+                          {cart.map((item) => (
+                            <div
+                              key={item.id}
+                              className="d-flex justify-content-between cart-item"
+                            >
+                              <img
+                                src={item.image}
+                                alt={item.title}
+                                className="cart-item-image"
+                              />
+                              <div className="cart-item-details">
+                                <p className="cart-item-title">{item.title}</p>
+                                <p className="cart-item-quantity">
+                                  Quantity: {item.quantity}
+                                </p>
+                                <p className="cart-item-price">
+                                  Price: ${item.price * item.quantity}
+                                </p>
+                              </div>
+                              <button
+                                onClick={() => handleRemoveItem(item.id)}
+                                className="remove-item-btn"
+                              >
+                                Remove
+                              </button>
+                            </div>
+                          ))}
+                        </div>
+                        <p className="cart-total">Total: ${cartTotal}</p>
+                        <button
+                          onClick={handleClearCart}
+                          className="btn btn-warning"
+                        >
+                          Clear Cart
+                        </button>
+                        <button type="button" className="btn btn-info">
+                          Checkout
+                        </button>
+                      </div>
+                    )}
+                  </MDBCol>
+
+                  <MDBCol lg="5">
+                    <MDBCard className="bg-primary text-white rounded-3">
+                      <MDBCardBody>
+                        <div className="d-flex justify-content-between align-items-center mb-4">
+                          <MDBTypography tag="h5" className="mb-0">
+                            Card details
+                          </MDBTypography>
+                        </div>
+
+                        <p className="small">Card type</p>
+                        <a href="#!" type="submit" className="text-white">
+                          <MDBIcon fab icon="cc-mastercard fa-2x me-2" />
+                        </a>
+                        <a href="#!" type="submit" className="text-white">
+                          <MDBIcon fab icon="cc-visa fa-2x me-2" />
+                        </a>
+                        <a href="#!" type="submit" className="text-white">
+                          <MDBIcon fab icon="cc-amex fa-2x me-2" />
+                        </a>
+                        <a href="#!" type="submit" className="text-white">
+                          <MDBIcon fab icon="cc-paypal fa-2x me-2" />
+                        </a>
+
+                        <form className="mt-4">
+                          <MDBInput
+                            className="mb-4"
+                            label="Cardholder's Name"
+                            type="text"
+                            size="lg"
+                            placeholder="Cardholder's Name"
+                            contrast
+                          />
+
+                          <MDBInput
+                            className="mb-4"
+                            label="Card Number"
+                            type="text"
+                            size="lg"
+                            minLength="19"
+                            maxLength="19"
+                            placeholder="1234 5678 9012 3457"
+                            contrast
+                          />
+
+                          <MDBRow className="mb-4">
+                            <MDBCol md="6">
+                              <MDBInput
+                                className="mb-4"
+                                label="Expiration"
+                                type="text"
+                                size="lg"
+                                minLength="7"
+                                maxLength="7"
+                                placeholder="MM/YYYY"
+                                contrast
+                              />
+                            </MDBCol>
+                            <MDBCol md="6">
+                              <MDBInput
+                                className="mb-4"
+                                label="Cvv"
+                                type="text"
+                                size="lg"
+                                minLength="3"
+                                maxLength="3"
+                                placeholder="&#9679;&#9679;&#9679;"
+                                contrast
+                              />
+                            </MDBCol>
+                          </MDBRow>
+                        </form>
+
+                        <hr />
+
+                        <div className="d-flex justify-content-between">
+                          <p className="mb-2">Subtotal</p>
+                          <p className="mb-2">${cartTotal}</p>
+                        </div>
+
+                        <div className="d-flex justify-content-between">
+                          <p className="mb-2">Shipping</p>
+                          <p className="mb-2">$0</p>
+                        </div>
+
+                        <div className="d-flex justify-content-between">
+                          <p className="mb-2">Total(Incl. taxes)</p>
+                          <p className="mb-2">${cartTotal}</p>
+                        </div>
+
+                        <MDBBtn color="info" block size="lg">
+                          <div className="d-flex justify-content-between">
+                            <span>${cartTotal}</span>
+                            <span>
+                              Checkout{" "}
+                              <i className="fas fa-long-arrow-alt-right ms-2"></i>
+                            </span>
+                          </div>
+                        </MDBBtn>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </section>
   );
 }
