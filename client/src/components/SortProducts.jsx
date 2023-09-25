ac516b1cb7ceb46a2a0fcd200795b30013f45fe2
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-09-25 01:59:16 -0500

    removed sort methods as it is now on ProductListPage

--- client/src/components/SortProducts.jsx
+++ client/src/components/SortProducts.jsx
@@ -1,40 +1,17 @@
-import { useEffect, useState } from "react";
-
-export default function SortProducts({ onSortChange, setProducts }) {
-  const [sortMethod, setSortMethod] = useState("none");
-
+export default function SortProducts({ onSortChange }) {
   const handleSortChange = (method) => {
-    setSortMethod(method);
     onSortChange(method);
   };
-
-  useEffect(() => {
-    const fetchSortedProducts = async () => {
-      try {
-        const response = await fetch(
-          `https://fakestoreapi.com/products?sort=${sortMethod}`
-        );
-        const result = await response.json();
-        setProducts(result);
-        console.log(result);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    if (sortMethod !== "none") {
-      fetchSortedProducts();
-    }
-  }, [sortMethod, setProducts]);
 
   return (
     <div className="sortbar">
       <label>Sort by:</label>
       <select
-        value={sortMethod}
+        defaultValue="none"
         onChange={(e) => handleSortChange(e.target.value)}
       >
-        <option value="ascending">Ascending (a-z)</option>
-        <option value="descending">Descending (z-a)</option>
+        <option value="ascending">Ascending (A-Z)</option>
+        <option value="descending">Descending (Z-A)</option>
       </select>
     </div>
   );
