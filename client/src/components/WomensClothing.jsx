0f622ce508ec97aab11a2ab9053ac972366a445a
dominiquemc <85527930+dominiquemc@users.noreply.github.com>
2023-09-25 02:00:39 -0500

    added category, addToCart, cart props. Removed hardcoded category from response

--- client/src/components/WomensClothing.jsx
+++ client/src/components/WomensClothing.jsx
@@ -1,42 +1,53 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-export default function Women() {
-  const [womensProducts, setWomensProducts] = useState([]);
+export default function Women({ category, addToCart, cart }) {
+  const [womenProducts, setWomenProducts] = useState([]);
 
   useEffect(() => {
     const fetchWomenProducts = async () => {
       try {
         const response = await fetch(
-          "https://fakestoreapi.com/products/category/women's clothing"
+          `https://fakestoreapi.com/products/category/${category}`
         );
         const result = await response.json();
-        setWomensProducts(result);
+        setWomenProducts(result);
       } catch (err) {
         console.log(err);
       }
     };
     fetchWomenProducts();
-  }, []);
+  }, [category]);
+
+  // add womens product to cart
+  const handleAddToCart = (product) => {
+    addToCart(product);
+  };
 
   return (
     <div className="allProducts-list">
-      <h1>Women's Clothing</h1>
+      <h1 className="category-title">Women's Clothing</h1>
       <ul className="products">
-        {womensProducts.map((product) => (
+        {womenProducts.map((product) => (
           <main key={product.id} className="allProducts">
-            <Link to={`/products/${product.id}`}>
+            <Link to={`/products/${product.id}`} className="product-link">
               <img
                 src={product.image}
                 className="productImages"
                 alt="Product for sale"
               />
-              <li>{product.title}</li>
+              <li className="product-title">{product.title}</li>
             </Link>
-            <li>
-              {product.rating.rate} stars {product.rating.count} reviews
+            <li className="product-rating">
+              {product.rating.rate} stars ({product.rating.count} reviews)
             </li>
-            <li>${product.price}</li>
+            <li className="product-price">${product.price}</li>
+            <button
+              className="add-btn-main add"
+              onClick={() => handleAddToCart(product)}
+            >
+              Add to Cart
+            </button>
           </main>
         ))}
       </ul>
